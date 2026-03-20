dc8c29ad6b9b8922807512a772a6427ae018b014
Damian Dudycz <damiandudycz@yahoo.com>
2026-03-20 12:30:00 +0100

    Update copyright holder to MobiTouch Sp. Z O. O.

--- src/__main__.py
+++ src/__main__.py
@@ -1,6 +1,6 @@
 # __main__.py
 #
-# Copyright (c) 2026 Damian Dudycz
+# Copyright (c) 2026 MobiTouch Sp. Z O. O.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
